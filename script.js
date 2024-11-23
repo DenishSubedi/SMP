d7fa4e4be9a89169fb7fe0a7354b9b4e2ff3ccdc
Shadow Assassin <161732875+DenishSubedi@users.noreply.github.com>
2024-11-23 21:42:29 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-document.getElementById('join').addEventListener('click', function() {
-    alert('Welcome to our server! We hope you enjoy your stay!');
-});
-
-
-
+document.addEventListener('contextmenu', event => event.preventDefault());
+        audioElement.play();
